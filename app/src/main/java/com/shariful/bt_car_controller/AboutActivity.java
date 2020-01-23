31619a68e982860ab1ec3f03fc14ef184eab4cfe
shariful6 <shariful49046@gmail.com>
2020-01-23 21:23:29 +0600

    2nd update

--- app/src/main/java/com/shariful/bt_car_controller/AboutActivity.java
+++ app/src/main/java/com/shariful/bt_car_controller/AboutActivity.java
@@ -2,7 +2,10 @@ package com.shariful.bt_car_controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -11,5 +14,11 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setTitle("About Developer");
+
+
     }
-}
+
+
+    }
+
+
